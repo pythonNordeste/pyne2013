f3cabc882521df1010654f1e1c7b4b5a5b96ea0b
Renato Oliveira <renatooliveira.cin@gmail.com>
2013-03-29 19:40:29 -0300

    Added slots to context.

--- core/views.py
+++ core/views.py
@@ -1,12 +1,12 @@
 #encoding: utf-8
 
-from django.views.generic import CreateView, TemplateView
-from django.contrib import messages
-
+from django.views.generic import TemplateView
 from core.models import Sponsor
+from schedule.models import Slot
+
 
 class IndexView(TemplateView):
-    
+
     template_name = "index.html"
 
     def get_context_data(self, **kwargs):
@@ -17,9 +17,15 @@ class IndexView(TemplateView):
             type=Sponsor.SILVER_TYPE)
         context['bronze_sponsors'] = Sponsor.objects.filter(
             type=Sponsor.BRONZE_TYPE)
+        context['first_day_slots'] = Slot.objects.filter(
+            time__day='24'
+        ).order_by('time')
+        context['second_day_slots'] = Slot.objects.filter(
+            time__day='25'
+        ).order_by('time')
         return context
 
 
 class CallForPapersView(TemplateView):
-    
+
     template_name = "callforpapers.html"
