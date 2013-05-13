d4b76b4e0161b0d0bd75e6612adb05a607fbceca
Fábio Cerqueira <cerqueirasfabio@gmail.com>
2013-05-13 14:09:31 -0300

    Removido url para cursos

--- pyne2013/urls.py
+++ pyne2013/urls.py
@@ -11,7 +11,6 @@ urlpatterns = patterns(
     url(r'^admin/', include(admin.site.urls)),
     url(r'^talk/', include('schedule.urls')),
     url(r'^hoteis/', include('hotels.urls')),
-    url(r'^cursos/', include('courses.urls')),
     url('^pages/', include('django.contrib.flatpages.urls')),
     url(r'^(?P<url>.*)$', 'django.contrib.flatpages.views.flatpage'),
 )
